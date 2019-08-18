fb3d675996c64b429b40b5835e372a9ae244530b
varunon9 <varunon9@gmail.com>
2019-08-18 16:04:09 +0530

    added access control list (acl) to models

--- common/models/article.js
+++ common/models/article.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Article) {
+  Article.beforeRemote('create', function(context, instance, next) {
+    context.args.data.createdAt = new Date();
+    context.args.data.updatedAt = new Date();
+    context.args.data.userId = context.req.accessToken.userId;
+    next();
+  });
 
+  Article.beforeRemote('prototype.patchAttributes', function(context, instance, next) {
+    context.args.data.updatedAt = new Date();
+    next();
+  });
 };
--- common/models/custom-user.js
+++ common/models/custom-user.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function(Customuser) {
+module.exports = function(CustomUser) {
+  CustomUser.beforeRemote('create', function(context, instance, next) {
+    context.args.data.createdAt = new Date();
+    context.args.data.updatedAt = new Date();
+    next();
+  });
 
+  CustomUser.beforeRemote('prototype.patchAttributes', function(context, instance, next) {
+    context.args.data.updatedAt = new Date();
+    next();
+  });
 };
